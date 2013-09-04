43341a80d2e4f07064a8ab24f547b64d4fba4a75
Christian Briones <cwbriones@gmail.com>
2013-09-03 18:17:45 -0700

    Got ycm config file working

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -14,8 +14,7 @@
 # of this software dedicate any and all copyright interest in the
 # software to the public domain. We make this dedication for the benefit
 # of the public at large and to the detriment of our heirs and
-# successors. We intend this dedication to be an overt act of
-# relinquishment in perpetuity of all present and future rights to this
+# successors. We intend this dedication to be an overt act of # relinquishment in perpetuity of all present and future rights to this
 # software under copyright law.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
@@ -34,28 +33,15 @@ import ycm_core
 # These are the compilation flags that will be used in case there's no
 # compilation database set (by default, one is not set).
 # CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
-flags = [
-'-Wall',
-'-Werror',
-'-Wno-variadic-macros',
-# THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
-# language to use when compiling headers. So it will guess. Badly. So C++
-# headers will be compiled as C headers. You don't want that so ALWAYS specify
-# a "-std=<something>".
-# For a C project, you would set this to something like 'c99' instead of
-# 'c++11'.
-'-std=c++11',
-# ...and the same thing goes for the magic -x option which specifies the
-# language that the files to be compiled are written in. This is mostly
-# relevant for c++ headers.
-# For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-'c++',
-'-I',
-'.'
-'-I',
-'./include'
-]
+compile_flags = "-Wall -Werror -Wno-variadic-macros -std=c++11 -x c++"
+
+include_dirs = ". .. ./include ../include"
+include_flags = " -I " + " -I ".join(include_dirs.split())
+
+libs = "sfml-window sfml-graphics sfml-system"
+linker_flags = " -l " + " -l ".join(libs.split())
+
+flags = (compile_flags + include_flags + linker_flags).split()
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
 # compile_commands.json file to use that instead of 'flags'. See here for
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include "Game.h"
 
+#include <iostream>
+#include <exception>
+
 int main(int argc, const char *argv[])
 {
-    Game game;
-    int retval = game.run();
+    int retval = -1;
+
+    try {
+        Game game;
+        retval = game.run();
+    } catch (std::exception& e){
+        std::cout << "\nERROR: " << e.what();
+    }
 
     return retval;
 }
